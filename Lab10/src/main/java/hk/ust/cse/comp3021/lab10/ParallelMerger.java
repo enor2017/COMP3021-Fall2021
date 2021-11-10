5b2a6a60f9611b676da20db8a7870a319252388c
enor2017 <enor2017@163.com>
2021-11-10 11:02:52 +0800

    Done with lab10.

--- Lab10/src/main/java/hk/ust/cse/comp3021/lab10/ParallelMerger.java
+++ Lab10/src/main/java/hk/ust/cse/comp3021/lab10/ParallelMerger.java
@@ -1,10 +1,30 @@
 package hk.ust.cse.comp3021.lab10;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ParallelMerger {
     @FunctionalInterface
     public interface ThreadSafeCharacterWriter {
         void write(char ch);
     }
+
+    /**
+     * indicating which thread should print now.
+     */
+    private static int currentThread = 0;
+    /**
+     * total number of threads
+     */
+    private static int totalThread;
+
+
 
     /**
      * The worker class which is {@link Runnable}.
@@ -18,15 +38,56 @@ public class ParallelMerger {
      * Then after the merge, {@link ThreadSafeCharacterWriter} should contain: "hello!".
      * 'h' should be written by the first worker, 'e' should be written by the second worker and 'l' should be written by the first worker, so on and so forth.
      * <p>
-     * TODO complete this class to implement the above functionality.
      * This class should implement {@link Runnable} so that it can be run in a thread.
      */
-    private static class Worker {
+    private static class Worker implements Runnable {
+        private final String word;
+        private final ThreadSafeCharacterWriter writer;
+        private final int id;
+        /**
+         * All Worker instances should share same lock and condition.
+         */
+        private static Lock lock = new ReentrantLock();
+        private static Condition newWrite = lock.newCondition();
 
+
+        public Worker(String word, ThreadSafeCharacterWriter writer, int id) {
+            this.word = word;
+            this.writer = writer;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < word.length(); ++i) {
+                lock.lock();
+                if (currentThread == id) {
+                    writer.write(word.charAt(i));
+                    currentThread = (id + 1) % totalThread;
+//                    System.out.println("thread: " + id + ", writing: " + word.charAt(i));
+//                    System.out.println("nextPrintThread: " + currentThread);
+                    newWrite.signalAll();
+                } else {
+//                    System.out.println("thread: " + id + ", waiting....");
+                    try {
+                        newWrite.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    --i;
+                }
+                lock.unlock();
+            }
+        }
     }
 
     /**
-     * TODO This method does the merge in an array of string segments. Here is the desired procedure:
+     * An arrayList to store all threads created.
+     */
+    private static ArrayList<Thread> threads = new ArrayList<>();
+
+    /**
+     * This method does the merge in an array of string segments. Here is the desired procedure:
      * <ol>
      *     <li>Create workers and their threads, the number of threads should be the same as the length of array {@code segments}.</li>
      *     <li>Start all threads to merge the {@code segments} in parallel.</li>
@@ -40,6 +101,24 @@ public class ParallelMerger {
      *                     should be called 5 times with each character in the order.
      */
     public static void merge(String[] segments, ThreadSafeCharacterWriter resultWriter) throws InterruptedException {
-
+        totalThread = segments.length;
+        for (int i = 0; i < segments.length; ++i) {
+            var thread = new Thread(new Worker(segments[i], resultWriter, i));
+            thread.start();
+            threads.add(thread);
+        }
+        boolean allTerminated;
+        while (true) {
+            allTerminated = true;
+            for (var thread : threads) {
+                if (thread.getState() != Thread.State.TERMINATED) {
+                    allTerminated = false;
+                    break;
+                }
+//                System.out.println("" + thread.getId() + thread.getState());
+            }
+            if (allTerminated) break;
+        }
+        threads.clear();
     }
 }
